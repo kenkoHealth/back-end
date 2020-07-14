6d0a66b22b50b8c8bdb039123bddde8b73bb016f
Aaron <aarongillies88@gmail.com>
2020-07-14 17:48:14 -0400

    added basic test for main api endpoint, both tests passing

--- tests/baseTests/server.test.js
+++ tests/baseTests/server.test.js
@@ -1,1 +1,17 @@
 // Basic server test
+const request = require("supertest");
+const server = require("../../server");
+
+describe("Server", () => {
+  it("Runs a basic server test", () => {
+    expect(true).toBe(true);
+  });
+  it("Hits main endpoint correctly", () => {
+    return request(server)
+      .get("/")
+      .then((res) => {
+        expect(res.status).toBe(200);
+      });
+  });
+  it("Returns the correct message upon being hit", () => {});
+});
