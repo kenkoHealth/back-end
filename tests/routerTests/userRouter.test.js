d25178fc81d2901d9af67e2d56c32453a045806e
Aaron <aarongillies88@gmail.com>
2020-07-20 19:25:05 -0400

    added test for getting a specific user by id

--- tests/routerTests/userRouter.test.js
+++ tests/routerTests/userRouter.test.js
@@ -30,5 +30,25 @@ describe("Successfully returns an array of all users", () => {
     const resType = typeof getUsers.body;
     expect(getUsers.status).toBe(200);
     expect(getUsers.body).toBeType(resType, "object");
+    expect(getUsers.body.length).toBeGreaterThanOrEqual(0);
+  });
+});
+
+// Test getting a specific user by ID
+describe("Test get an individual user from database", () => {
+  it("Successfully returns an array of length 1 with a status code of 200", async () => {
+    let user = generateUser("kenkoTest2", "Andrew", "Smith");
+    let response = await request(server).post("/api/auth/register").send(user);
+
+    let login = await request(server).post("/api/auth/login").send({
+      email: user.email,
+      password: user.password,
+    });
+    const currentToken = login.body.token;
+    let getSingleUser = await request(server)
+      .get("/api/users/1")
+      .set("Authorization", currentToken);
+    expect(getSingleUser.status).toBe(200);
+    expect(getSingleUser.body.length).toEqual(1);
   });
 });
