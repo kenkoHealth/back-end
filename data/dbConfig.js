51e541c934be12c00a5e352cb518041b831cbffb
Aaron <aarongillies88@gmail.com>
2020-07-25 13:06:38 -0400

    switching dbconfig to test database

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,7 +2,7 @@ const knex = require("knex");
 
 const knexConfig = require("../knexfile");
 
-const dbEnv = process.env.DB_ENV || "development";
+const dbEnv = process.env.DB_ENV || "test";
 
 const database = knex(knexConfig[dbEnv]);
 
