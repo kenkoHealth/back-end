04ffcb3d954fe1eefb6b131970530286b15099ef
Aaron <aarongillies88@gmail.com>
2020-09-17 16:41:57 -0400

    built goals migration with ref to users table

---
+++ data/migrations/20200917161713_goals.js
@@ -0,0 +1,21 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("goals", (table) => {
+    table.increments();
+    table.string("goal_name", 255).notNullable();
+    table.string("goal_description", 255).notNullable();
+    table.date("goal_date").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table
+      .integer("goal_user")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("users")
+      .onUpdate("CASCADE")
+      .onDelete("RESTRICT");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("goals");
+};
