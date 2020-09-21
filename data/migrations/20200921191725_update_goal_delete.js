123fe77c6421e03c1051bde62e72ccd65763d4bd
Aaron <aarongillies88@gmail.com>
2020-09-21 19:26:23 -0400

    added migration to replace restrict delete to cascade, this allows us to delete a user even if said user is referenced by a goal

---
+++ data/migrations/20200921191725_update_goal_delete.js
@@ -0,0 +1,15 @@
+exports.up = function (knex) {
+  return knex.schema.alterTable("goals", (table) => {
+    table.dropForeign("goal_user");
+
+    table.foreign("goal_user").references("users.id").onDelete("CASCADE");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.alterTable("goals", (table) => {
+    table.dropForeign("goal_user");
+
+    table.foreign("goal_user").references("users.id").onDelete("NO ACTION");
+  });
+};
