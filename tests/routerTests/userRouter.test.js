ff8de2540140e8caa68d19ebe6a9a17acb8a8d94
Aaron <aarongillies88@gmail.com>
2020-09-26 11:51:00 -0400

    abstracted fake user generation and authentication away from router file into a utils folder for tests

--- tests/routerTests/userRouter.test.js
+++ tests/routerTests/userRouter.test.js
@@ -1,30 +1,9 @@
+import { authenticateForTest } from "../testUtils/utilityUser";
+
 // User Routes tests
 const request = require("supertest");
 const server = require("../../server");
 
-// Utility function to generate test users
-function generateUser(pass, first, last) {
-  const randomUserNum = Math.random() * 5000;
-  let testUser = {
-    email: `kenkoTest${randomUserNum}@email.com`,
-    password: pass,
-    first_name: first,
-    last_name: last,
-  };
-  return testUser;
-}
-
-// Could write another utility function to register/login a user to help clean up test code a bit
-async function authenticateForTest() {
-  const user = generateUser("KenkoTest5", "Erica", "Sims");
-  const response = await request(server).post("/api/auth/register").send(user);
-
-  const login = await request(server).post("/api/auth/login").send({
-    email: user.email,
-    password: user.password,
-  });
-  return login;
-}
 // Test get all users endpoint. (Routes are protected, will need to register and login in order to access)
 describe("Successfully returns an array of all users", () => {
   it("Successfully returns status code 200 on request", async () => {
