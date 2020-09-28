8738e9c068ea22d495f4b63fc59142178713eb6c
Aaron <aarongillies88@gmail.com>
2020-09-28 17:27:04 -0400

    added basic test for retrieving array of all goals from DB

--- tests/routerTests/goalsRouter.test.js
+++ tests/routerTests/goalsRouter.test.js
@@ -1,1 +1,15 @@
-// Testing for Goals Router will live here
+const testUtils = require("../testUtils/utilityUser");
+const request = require("../../server");
+const server = require("supertest");
+
+// Test If we can receive all goals in the database
+describe("Successfully returns an array of all goals in the database", () => {
+  it("Successfully returns a status code 200", async () => {
+    const authenticated = await testUtils.authenticateForTest();
+    const currentToken = authenticated.body.token;
+    const goals = await request(server)
+      .get("/api/goals/")
+      .set("cookie", currentToken);
+    expect(goals.status).toBe(200);
+  });
+});
