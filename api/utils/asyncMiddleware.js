c6fc87ec4fc9e0fb6c7ff1869309aeb02df64abc
Aaron <aarongillies88@gmail.com>
2020-09-27 20:29:57 -0400

    updated verbage for asyncMW function in comments

--- api/utils/asyncMiddleware.js
+++ api/utils/asyncMiddleware.js
@@ -1,3 +1,4 @@
+// Will eventually utilize this to handle catching errors, will check for promise success/failure and move to the next MW.
 const asyncMiddleware = (fn) => (req, res, next) => {
   Promise.resolve(fn(req, res, next)).catch(next);
 };
