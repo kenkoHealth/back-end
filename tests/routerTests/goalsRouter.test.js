94bbc29d7ac2f61b9123264c7eef943c785edb1a
Aaron <aarongillies88@gmail.com>
2020-09-29 15:53:57 -0400

    added successful tests for retrieving all goals in DB as well as getting all goals for a specific user, also added tests to ensure proper error output

--- tests/routerTests/goalsRouter.test.js
+++ tests/routerTests/goalsRouter.test.js
@@ -11,6 +11,32 @@ describe("Successfully returns an array of all goals in the database", () => {
       .get("/api/goals/")
       .set("Cookie", currentToken);
     expect(goals.status).toBe(200);
+    expect(goals.body).toBeDefined();
     expect(goals.body).toBeArray();
+  });
+});
+
+// Test retrieving all goals for a specific user by specifying the user ID
+describe("Successfully returns an array of all goals for a specific user", () => {
+  it("Successfully returns an array of goals, and status code 200", async () => {
+    const authenticated = await testUtils.authenticateForTest();
+    const currentToken = authenticated.body.token;
+    const goals = await request(server)
+      .get(`/api/goals/${authenticated.body.current_user.id}`)
+      .set("Cookie", currentToken);
+    expect(goals.status).toBe(200);
+    expect(goals.body).toBeDefined();
+    expect(goals.body).toBeArray();
+  });
+  it("Request fails and returns status 401 if cookie is not sent in headers", async () => {
+    const authenticated = await testUtils.authenticateForTest();
+    const goals = await request(server).get(
+      `/api/goals/${authenticated.body.current_user.id}`
+    );
+    expect(goals.status).toBe(400);
+    expect(goals.body).toBeDefined();
+    expect(goals.body).toEqual({
+      message: `Please login and try again!`,
+    });
   });
 });
