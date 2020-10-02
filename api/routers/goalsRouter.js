4ccfd747db13bfce6391b6c911d14e1d37629567
Aaron <aarongillies88@gmail.com>
2020-10-02 17:43:48 -0400

    added reminder for fix
    Co-authored-by: bamfranceschi <anna.franceschi@gmail.com>

--- api/routers/goalsRouter.js
+++ api/routers/goalsRouter.js
@@ -64,6 +64,7 @@ router.post("/", authmw, async (req, res) => {
         });
       }
     }
+    // Issue was only returning a 200 and result if the initial 'if' in for loop was failing. Date: 10-2-2020
     res.status(200).json(result);
   } catch (e) {
     res.status(500).json({ error: e.response, message: `Unable to add Goal.` });
