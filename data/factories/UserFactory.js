f987a4a646282b2fee7cac737a92803ba953a94f
Aaron <aarongillies88@gmail.com>
2021-03-13 12:52:16 -0500

    updated Authentication tests after changes to users DB table

--- data/factories/UserFactory.js
+++ data/factories/UserFactory.js
@@ -1,19 +1,15 @@
 // User Factory class to quickly seed database user faker data
 const { v4: uuidv4 } = require("uuid");
 const faker = require("faker");
-
 /* 
     - Base factory function to build a user instance
-    - We can pass the user_type and number of creations we want
+    - We can pass the user_type <-- To do -- and number of creations we want
     - Will return an array of user objects
 
 */
-
-// May need some kind of check to prevent attempting to build massive numbers of users at once
 function createUsers(instances) {
   let iterations = 0;
   let createdUsersArray = [];
-  // While we haven't built all our instances
   while (iterations < instances) {
     createdUsersArray.push({
       id: uuidv4(),
--- tests/routerTests/authRouter.test.js
+++ tests/routerTests/authRouter.test.js
@@ -1,33 +1,19 @@
 const request = require("supertest");
 const server = require("../../server");
-//
-// Test Register endpoint
+const userUtility = require("../testUtils/utilityUser");
 
 // Test successful user registration.
 describe("User should be able to successfully register", () => {
   it("Should successfully register a user and return status code 201", async () => {
-    const randomUserNum = Math.random() * 5000;
     let response = await request(server)
       .post("/api/auth/register")
-      .send({
-        email: `KenkoTest${randomUserNum}@email.com`,
-        password: "KenkoTest",
-        first_name: "Aaron",
-        last_name: "Gillies",
-      });
+      .send(userUtility.generateUser());
     expect(response.status).toBe(201);
   });
   it("Should return a valid token", async () => {
-    const randomUserNum = Math.random() * 5000;
     let response = await request(server)
       .post("/api/auth/register")
-      .send({
-        email: `KenkoTest${randomUserNum}@email.com`,
-        password: "KenkoTest",
-        first_name: "Aaron",
-        last_name: "Gillies",
-      });
-    let tokenType = typeof response.body.token;
+      .send(userUtility.generateUser());
     expect(response.body.token).toBeUndefined();
   });
   it("Should fail if incorrect request body is sent", async () => {
@@ -41,17 +27,10 @@ describe("User should be able to successfully register", () => {
 });
 
 // Test successful user login
-
 describe("User should be able to successfully login after registering", () => {
   it("Should successfully register and login returning status 200, as well as valid token", async () => {
     // Need to register a user here before we login.
-    const randomUserNum = Math.random() * 5000;
-    let testUser = {
-      email: `KenkoTest${randomUserNum}@email.com`,
-      password: "KenkoTest",
-      first_name: "Aaron",
-      last_name: "Gillies",
-    };
+    let testUser = userUtility.generateUser();
     let response = await request(server)
       .post("/api/auth/register")
       .send(testUser);
@@ -69,13 +48,7 @@ describe("User should be able to successfully login after registering", () => {
     );
   });
   it("Should return a status code 401 if user is not properly authenticated", async () => {
-    const randomUserNum = Math.random() * 5000;
-    let testUser = {
-      email: `KenkoTest${randomUserNum}@email.com`,
-      password: "KenkoTest",
-      first_name: "Aaron",
-      last_name: "Gillies",
-    };
+    let testUser = userUtility.generateUser();
     let login = await request(server).post("/api/auth/login").send({
       email: "kenkotest@email.com",
       password: testUser.password,
