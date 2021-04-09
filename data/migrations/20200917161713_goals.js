1849508a4252fd8e080a27f3129fafd26db3ac33
Aaron <aarongillies88@gmail.com>
2021-04-09 19:24:00 -0400

    Updated user migrations and correctly imported UUID PG library to assign correct primary keys to database records

--- data/migrations/20200917161713_goals.js
+++ data/migrations/20200917161713_goals.js
@@ -1,19 +1,25 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("goals", (table) => {
-    table.uuid("id").primary();
-    table.string("goal_name", 255).notNullable();
-    table.string("goal_description", 255).notNullable();
-    table.date("goal_date").notNullable();
-    table.timestamp("created_at").defaultTo(knex.fn.now());
-    table
-      .uuid("goal_user")
-      .unsigned()
-      .notNullable()
-      .references("id")
-      .inTable("users")
-      .onUpdate("CASCADE")
-      .onDelete("RESTRICT");
-  });
+  return knex.schema
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable("goals", (table) => {
+      table
+        .uuid("id")
+        .primary()
+        .notNullable()
+        .defaultTo(knex.raw("uuid_generate_v4()"));
+      table.string("goal_name", 255).notNullable();
+      table.string("goal_description", 255).notNullable();
+      table.date("goal_date").notNullable();
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table
+        .uuid("goal_user")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onUpdate("CASCADE")
+        .onDelete("RESTRICT");
+    });
 };
 
 exports.down = function (knex) {
--- data/migrations/20200921191725_update_goal_delete.js
+++ data/migrations/20200921191725_update_goal_delete.js
@@ -1,15 +1,13 @@
 exports.up = function (knex) {
-  return knex.schema.alterTable("goals", (table) => {
-    table.dropForeign("goal_user");
-
-    table.foreign("goal_user").references("users.id").onDelete("CASCADE");
-  });
+  //   return knex.schema.alterTable("goals", (table) => {
+  //     table.dropForeign("goal_user");
+  //     table.foreign("goal_user").references("users.id").onDelete("CASCADE");
+  //   });
 };
 
 exports.down = function (knex) {
-  return knex.schema.alterTable("goals", (table) => {
-    table.dropForeign("goal_user");
-
-    table.foreign("goal_user").references("users.id").onDelete("NO ACTION");
-  });
+  //   return knex.schema.alterTable("goals", (table) => {
+  //     table.dropForeign("goal_user");
+  //     table.foreign("goal_user").references("users.id").onDelete("NO ACTION");
+  //   });
 };
