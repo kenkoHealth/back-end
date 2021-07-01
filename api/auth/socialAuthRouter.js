b0d15ef385dfc6a18f1be356cd8a66e5aa365628
Aaron <aarongillies88@gmail.com>
2021-07-01 12:27:56 -0400

    base router set up for passing code to call to Facebook graphQL API which sshould return an access token we can pass back to the client for calls to fetch user profile data

--- api/auth/socialAuthRouter.js
+++ api/auth/socialAuthRouter.js
@@ -7,4 +7,19 @@ const router = express.Router();
 require("dotenv").config();
 
 // Fetch access token with authentication code received by the front-end
-router.get("https://graph.facebook.com/v11.0/oauth/access_token?");
+router.get("/facebook/token", async (req, res) => {
+  // Pull the accessCode out of the request object
+  const accessCode = req.code;
+  // Make request to Facebook GraphQL endpoint passing all the required data and the code above
+  let response = await axios.get(
+    `https://graph.facebook.com/v11.0/oauth/access_token?client_id=${process.env.FB_APP_ID}&redirect_uri=${process.env.FB_REDIRECT_URI}&client_secret=${process.env.FB_APP_SECRET}&code=${accessCode}`
+  );
+  // Log response from Facebook
+  console.log(response);
+  // Should look like...
+  //   {
+  //     "access_token": {access-token},
+  //     "token_type": {type},
+  //     "expires_in":  {seconds-til-expiration}
+  //   }
+});
