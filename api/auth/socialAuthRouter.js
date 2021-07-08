6983dc8a2bc31a3cf584e038c206654dd9948d91
Aaron <aarongillies88@gmail.com>
2021-07-08 15:19:20 -0400

    getting proper data back from FB api, just need to generate a user in our database based on this information and some fake scaffolded data required for db that user can directly modify from the app later on

--- api/auth/socialAuthRouter.js
+++ api/auth/socialAuthRouter.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const Users = require("../models/userModels");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 const router = express.Router();
 const axios = require("axios");
+const faker = require("faker");
 require("dotenv").config();
 
 router.post("/facebook/token", async (req, res) => {
@@ -21,9 +20,23 @@ router.post("/facebook/token", async (req, res) => {
       `https://graph.facebook.com/v11.0/${id}?fields=email,first_name,last_name,picture,id&access_token=${response.data.access_token}`
     );
     console.log(userResponse, "userData");
+
     // Use the userData that comes from FB to create a user in our database
     /* Make sure to check if the user already exists in the database, if it does....simply fetch and return. 
     Otherwise, generate a new user and return */
+
+    let userData = userResponse.data;
+    userToAdd = {
+      email: userData.email,
+      password: faker.internet.password(10),
+      username: `${userData.first_name}.${userData.last_name}`,
+      first_name: userData.first_name,
+      last_name: userData.last_name,
+    };
+
+    console.log(userToAdd, "combined user data to be posted to DB");
+
+    /* END */
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error, message: error.message });
