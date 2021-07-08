39e739b7bef28be5b338166ff8afabea765d63c2
Aaron <aarongillies88@gmail.com>
2021-07-08 11:41:54 -0400

    successfully fetching user id from facebook API, just need to make request to get all the relevant user data so we can generate a user in the database with the data

--- api/auth/socialAuthRouter.js
+++ api/auth/socialAuthRouter.js
@@ -14,15 +14,12 @@ router.post("/facebook/token", async (req, res) => {
     let response = await axios.get(
       `https://graph.facebook.com/v11.0/oauth/access_token?client_id=${process.env.FB_APP_ID}&redirect_uri=${process.env.FB_REDIRECT_URI}&client_secret=${process.env.FB_APP_SECRET}&code=${accessCode}`
     );
-    // Log response from Facebook
-    console.log(response);
-    /* Need to figure out what to do next here...
-    We can either handle the accessToken in the server, make request to fetch user data and generate an account based on that */
+    let fbUserId = await axios.get(
+      `https://graph.facebook.com/me?fields=id&access_token=${response.data.access_token}`
+    );
+    const { id } = fbUserId.data;
 
-    /* OR we can pass the token back to the front-end where they can use it to make requests to fetch user data to persist to the client */
-
-    // Now I'm thinking I should get the user data, build a user and add to database, and then return that user data for the FE to persist,
-    // It's just safer to handle it all on the server and only pass the client what it absolutely needs
+    // Make request to fetch all the user data
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error, message: error.message });
